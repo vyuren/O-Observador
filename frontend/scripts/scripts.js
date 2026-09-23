f3bc638025ebc5910dfd713bd783af7dc545e94c
vyuren <Anasemporsemto@gmail.com>
2026-09-23 16:19:06 -0300

    added dark mode for index

---
+++ frontend/scripts/scripts.js
@@ -0,0 +1,35 @@
+const themechanger = document.getElementById("themechanger");
+const body = document.body;
+const board = document.getElementById("board");
+const p = document.querySelectorAll("p");
+let isDarkMode = false;
+
+themechanger.addEventListener("click", function () {
+
+    isDarkMode = !isDarkMode;
+
+    if (isDarkMode == true) {
+
+        themechanger.src = "assets/darkmode.webp";
+        body.style.backgroundColor = "rgb(26, 26, 26)";
+        board.style.backgroundColor = "rgb(48, 48, 48)";
+
+        p.forEach(function(p){
+
+            p.style.color = "rgb(255, 255, 255)";
+        })
+        
+
+    } else {
+
+        themechanger.src = "assets/lightmode.webp";
+        body.style.backgroundColor = "white";
+        board.style.backgroundColor = "rgb(224, 224, 224)";
+
+        p.forEach(function(p){
+
+            p.style.color = "black";
+        })
+        
+    }
+})
